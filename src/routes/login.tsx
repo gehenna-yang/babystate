199e17e18bdc4e5424817862b5d7ffd0f57b6c15
gehenna-yang <winer0707@naver.com>
2026-04-21 15:02:28 +0900

    modify

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -6,7 +6,6 @@ export const Route = createFileRoute('/login')({
     beforeLoad: () => {
         const { accessToken } = useAuthStore.getState()
     
-        // ✅ 이미 로그인한 사람이 로그인 페이지에 오면 대시보드로 보냅니다.
         if (accessToken) {
           throw redirect({ to: '/dashboard' }) 
         }
