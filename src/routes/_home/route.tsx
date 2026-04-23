52e071e8f1aabc6e310c29989e48f18a4f94f9c3
gehenna-yang <winer0707@naver.com>
2026-04-23 11:09:33 +0900

    modify

--- src/routes/_home/route.tsx
+++ src/routes/_home/route.tsx
@@ -4,14 +4,12 @@ import { NavBarLayOut } from '../../pages/navbar/navBarLayout'
 
 export const Route = createFileRoute('/_home')({
   beforeLoad: ({ location }) => {
-    const { accessToken } = useAuthStore.getState() // 스토어에서 로그인 상태 가져오기
+    const { accessToken } = useAuthStore.getState()
   
     if (!accessToken) {
-      // 로그인이 안 되어 있다면 login 페이지로 강제 이동
       throw redirect({
         to: '/login',
         search: {
-          // 로그인 후 다시 돌아올 주소를 저장해두면 센스 만점!
           redirect: location.href, 
         },
       })
